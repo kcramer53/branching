849858fceb26247a4cdaabb65206570454fd4c51
Kirsten Putman and Susan Kralian <kirsten@>
2019-08-15 10:17:30 -0700

    add functional quiz with button and answer modify jquery css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 
 
-var flavor = $("input:radio[name=pet]:checked").val();
+$(document).ready(function(){
+  $("#analysis").click(function(){
+      var pet = $("input:radio[name=pet]:checked").val();
+      if (pet==="cookie1") {
+        $("#scooby2").hide();
+        $("#imagine2").hide();
+        $("#cookie2").show();
+      }
+      else if (pet==="scooby1") {
+        $("#imagine2").hide();
+        $("#cookie2").hide();
+        $("#scooby2").show();
+      }
+      else {
+        $("#scooby2").hide();
+        $("#cookies2").hide();
+        $("#imagine2").show();
+      }
+      event.preventDefault();
+  });
+});
