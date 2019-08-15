efc361307c94055447aa8806d219bd4c0cf87e77
Kirsten Putman and Susan Kralian <susan@>
2019-08-15 15:08:06 -0700

    reworks the checkboxes and quiz execution

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,16 @@ $(document).ready(function(){
     console.log(age);
     var desire = $("input:checkbox[name=desire]:checked").val();
     console.log(desire);
-    // if (age >= 18 && desire = ""      ) {
-    //
-    // }
-
-
-});
+    if (age >= 18 && desire==="Y") {
+      $("#pass-age").show();
+    } else if (age<18) {$(".screen").show("Sorry you need to be 18 to adopt a furry friend. Come back when you are older!");
+      }
+      else if (desire==="N"){$("#no-desire").show();
+      } else {
+        alert('Please enter your age.');
+    }
+  });
+  // alert is NOT working. too young screen is NOT working(else if <18). 
 
   $("#button").click(function(){
       var pet = $("input:radio[name=pet]:checked").val();
